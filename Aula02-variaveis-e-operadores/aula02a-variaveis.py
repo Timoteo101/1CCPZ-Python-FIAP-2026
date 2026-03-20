7209550793b7edccd048c61bfb30de58ac88db42
Timoteo101 <timoteo.romano2007@gmail.com>
2026-03-20 20:50:25 -0300

    aula 2 variaveis e operadores

---
+++ Aula02-variaveis-e-operadores/aula02a-variaveis.py
@@ -0,0 +1,30 @@
+print("Hello World")
+
+
+
+
+print("7 + 4")
+print("7" + "4")
+print(7 + 4)
+
+
+
+
+
+# variaveis
+nome = "Bruno"  # string
+idade = 26 # int
+peso = 70.2 # float
+
+print( nome, idade, peso)
+print(f"oiii, {nome}!!!!")
+
+#input -- simula um forms no cmd
+nome = input(" Digite o seu nome: ")
+idade = int(input(" Digite a sua idade: "))    # converte string para int
+
+nova_idade = idade + 1
+
+
+print(nome , idade)
+print(nova_idade)
---
+++ desafio 2.py
@@ -0,0 +1,5 @@
+dia = input("Dia de nascimento:")
+mes = input("Mês de Nascimento:")
+ano = input(" Ano de nascimanto:")
+
+print(dia+"/"+mes+"/"+ano)
