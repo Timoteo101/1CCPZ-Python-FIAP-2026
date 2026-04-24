f3d7e6caa84433de0bb366c47c5e131c0a3ccb01
Timoteo101 <timoteo.romano2007@gmail.com>
2026-04-24 20:49:56 -0300

    aula04

---
+++ Aula 4- While.py
@@ -0,0 +1,12 @@
+i = 0
+while i <10:
+    i+=1
+
+    if i == 3 or i == 5:
+        continue
+
+    if i == 7:
+        break
+
+    print(f"produto {i}")
+
---
+++ aula04b-for.py
@@ -0,0 +1,18 @@
+#for count_music in range(3):
+ #   print(f"Música {count_music}")
+
+# de 1 a 11, pulando de 2 em 2
+#for i in range(1, 12, 2):
+  #  print(i)
+
+    # atividade 3
+
+#qtd_mus = int(input("Digite a qntd de mus da sua plst (DB)"))
+# i in range(qtd_mus):
+ #   print(f"Musica {i}")
+
+ #LAÇOS ANINHADOS
+ #rEP. ENCADEADA
+for i in range(0, 4):
+    for j in range( 0, 3, 2):
+        print(f"i: {i}, j:{j}")
